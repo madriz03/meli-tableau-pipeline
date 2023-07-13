ca52015ef785aec8a4ec97ca4632c6bb862899b6
Javier Madriz <javiermadriz3@gmail.com>
2023-07-13 15:15:32 -0500

    Se agrego documentacion, pendiente de test

--- main.py
+++ main.py
@@ -6,30 +6,56 @@ from settings import COLUMN_NAMES
 from validations import validate_search_term
 
 
+
 def get_items(url, search_term, n_items):
+    """get_items Retrieves items from an API based on the provided URL, search term, and number of items.
+
+    Parameters:
+        url (str): The URL of the API to retrieve items from.
+        search_term: The search term to use for querying the API.
+        n_items (int): The total number of items to retrieve.
+
+    offset (int): The offset value to specify the starting point for retrieving items
+
+    Raises:
+        requests.exceptions.RequestException: If an error occurs during the API request.
+
+    Returns:
+        list: A list of items retrieved from the API.
+        """
     items = []
     offset = 0
-    while offset < n_items:
-        params = {
-            'query': search_term,
-            'offset': offset,
-            'limit': min((n_items - len(items), 50))
-        }
+    try:
+        while offset < n_items:
+            params = {
+                'query': search_term,
+                'offset': offset,
+                'limit': min((n_items - len(items), 50))
+            }
 
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
             data = response.json()
             results = data.get('results', [])
             items += results
             offset += len(results)
 
-        else:
-            print('error')
+    except requests.exceptions.RequestException as e:
+        print(f'Error occurred during API request: {str(e)}')
         
     return items
     
 
 def create_hyper_table_definition():
+    """
+    Creates the table structure for the Hyper file.
+    
+    This function defines the table name, column names, and data types for each column.
+    
+    Returns:
+        The table structure definition.
+    """
+    
     table = TableDefinition(TableName('data_meli', 'data_meli'), [
         TableDefinition.Column('id', SqlType.text()),
         TableDefinition.Column('title', SqlType.text()),
@@ -42,6 +68,22 @@ def create_hyper_table_definition():
 
 
 def insert_items_table(connection, table, items, column_name):
+    """
+    Inserts items into a table in the Hyper file using the provided connection, table, items, and column names.
+    
+    Parameters:
+        connection (tableauhyperapi.Connection): The connection to the Hyper file.
+        table (tableauhyperapi.TableDefinition): The table definition of the target table.
+        items (list): A list of items to be inserted into the table.
+        column_name (list): A list of column names corresponding to the items' attributes.
+    
+    Returns:
+        None
+    
+    Exeption:
+        except if exist a problem during insertion of items
+    
+    """
     with Inserter(connection, table) as inserter:
         try:
             for item in items:
@@ -53,6 +95,24 @@ def insert_items_table(connection, table, items, column_name):
 
 
 def hyper_structure_generator(search_term):
+    """
+    Main function that generates the Hyper file structure by querying the API, creating a table, and inserting data.
+    
+    This function serves as the entry point and makes use of the following functions:
+    - get_items: Retrieves items from the API based on the search term.
+    - create_hyper_table_definition: Creates the table structure for the Hyper file.
+    - insert_items_table: Inserts the retrieved items into the table.
+    
+    Parameters:
+        search_term (str): The search term to use for querying the API.
+        
+    Returns:
+        str: The local path of the generated Hyper file.
+    
+    Raises:
+        None
+    
+    """
     local_path = os.path.join(os.getcwd(), 'public_data_meli.hyper')
     with HyperProcess(Telemetry.SEND_USAGE_DATA_TO_TABLEAU,
                        parameters={"default_database_version": "2"}) as hyper:
@@ -72,7 +132,8 @@ def hyper_structure_generator(search_term):
         return local_path
     
 
-search_term = input('What do you want to search: ')
-if validate_search_term(search_term):
-    hyper_path = hyper_structure_generator(search_term)
-    print(f'Executed successfully, the hyper file is in the path {hyper_path}')
+if __name__ == '__main__':
+    search_term = input('What do you want to search: ')
+    if validate_search_term(search_term):
+        hyper_path = hyper_structure_generator(search_term)
+        print(f'Executed successfully, the hyper file is in the path {hyper_path}')
